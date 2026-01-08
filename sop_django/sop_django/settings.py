8fbd863a72aae4433e25fcee7d9c548d6656ebdb
VIshal6620 <vishalvish7099@gmail.com>
2026-01-08 18:09:20 +0530

    done

--- sop_django/sop_django/settings.py
+++ sop_django/sop_django/settings.py
@@ -72,13 +72,21 @@ WSGI_APPLICATION = 'sop_django.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'adjango_5',
+#         'USER': 'root',
+#         'PASSWORD': 'root',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'adjango_5',
         'USER': 'root',
         'PASSWORD': 'root',
-        'HOST': '127.0.0.1',
+        'HOST': 'db',
         'PORT': '3306',
     }
 }
