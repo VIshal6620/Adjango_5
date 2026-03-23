8a8c724c885649c76d6bb921d57a91a237363f69
VIshal6620 <vishalvish7099@gmail.com>
2026-03-23 17:06:44 +0530

    23-03-2026

--- sop_django/sop_django/settings.py
+++ sop_django/sop_django/settings.py
@@ -156,4 +156,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'vishalvish7099@gmail.com'
-EMAIL_HOST_PASSWORD = 'jfvi wwhb ncoy yyqg'
+EMAIL_HOST_PASSWORD = 'kdht jdyr uhst'
